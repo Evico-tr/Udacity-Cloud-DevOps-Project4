2bbb8a2f204d3defa358c28c8aa4fd5b43071c1e
EC2 Default User <ec2-user@ip-172-31-8-110.ec2.internal>
2022-09-05 21:17:01 +0000

    updated kubernetes_out.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="evico/ml-predict:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-predict --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-predict 8000:80
